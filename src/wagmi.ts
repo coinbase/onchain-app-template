44326f3df23391ae2bcd636a67aaff73c76723c8
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-06-12 19:51:24 -0700

    fix: 4

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -10,6 +10,7 @@ export const wagmiConfig = createConfig({
     coinbaseWallet({
       appName: 'anOnchainAppIn100Components',
       preference: 'all',
+      version: '4',
     }),
   ],
   ssr: true,
