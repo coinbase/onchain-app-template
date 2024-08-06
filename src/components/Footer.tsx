27c109027deda703839156b7242c902cff03f4f8
Alissa Crane <alissa.crane@coinbase.com>
2024-08-05 22:24:36 -0700

    refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,17 @@
 
 import BaseSvg from './BaseSvg';
 
+const docLinks = [
+  { href: 'https://onchainkit.xyz', title: 'Docs' },
+  { href: 'https://github.com/coinbase/onchainkit', title: 'Github' },
+  { href: 'https://discord.gg/8gW3h6w5', title: 'Discord' },
+  {
+    href: 'https://www.figma.com/community/file/1370194397345450683/onchainkit',
+    title: 'Figma',
+  },
+  { href: 'https://x.com/Onchainkit', title: 'X' },
+];
+
 export default function Footer() {
   return (
     <section className="mt-12 mb-6 flex w-full justify-between">
@@ -11,43 +22,13 @@ export default function Footer() {
         <h3 className="ml-1 text-[#0052FF] text-m">Base</h3>
       </aside>
       <ul className="flex gap-6">
-        <li className="flex">
-          <a href="https://onchainkit.xyz" target="_blank" rel="noreferrer">
-            Docs
-          </a>
-        </li>
-        <li className="flex">
-          <a
-            href="https://github.com/coinbase/onchainkit"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-        </li>
-        <li className="flex">
-          <a
-            href="https://discord.gg/8gW3h6w5"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Discord
-          </a>
-        </li>
-        <li className="flex">
-          <a
-            href="https://www.figma.com/community/file/1370194397345450683/onchainkit"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Figma
-          </a>
-        </li>
-        <li className="flex">
-          <a href="https://x.com/Onchainkit" target="_blank" rel="noreferrer">
-            X
-          </a>
-        </li>
+        {docLinks.map(({ href, title }) => (
+          <li className="flex">
+            <a href={href} target="_blank" rel="noreferrer">
+              <p>{title}</p>
+            </a>
+          </li>
+        ))}
       </ul>
     </section>
   );
