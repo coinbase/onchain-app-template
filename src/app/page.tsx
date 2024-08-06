f2ad6c3a94cc27cf3a76da00c8412398ce7b8603
Alissa Crane <alissa.crane@coinbase.com>
2024-08-06 08:51:47 -0700

    fix hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Page() {
   const { address } = useAccount();
 
   return (
-    <div className='flex h-full w-96 max-w-full flex-col md:w-[1008px]'>
+    <div className="flex h-full w-96 max-w-full flex-col md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col md:flex-row">
-        <div className='flex w-full flex-col items-center justify-between gap-2 md:flex-row md:gap-0'>
+        <div className="flex w-full flex-col items-center justify-between gap-2 md:flex-row md:gap-0">
           <h2 className="text-3xl">Onchainkit</h2>
           <div className="flex gap-3">
             <SignupButton />
@@ -21,7 +21,7 @@ export default function Page() {
         </div>
       </section>
       <section className="templateSection flex w-full grow flex-col items-center justify-center gap-4 rounded-xl md:bg-gray-100">
-        <div className='flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]'>
+        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
               npm install @coinbase/onchainkit
