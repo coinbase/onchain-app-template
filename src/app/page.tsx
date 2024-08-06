b33cf9a620a4d13b90ca14e49b5c6b7adbbed973
Alissa Crane <alissa.crane@coinbase.com>
2024-08-05 21:41:02 -0700

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   return (
     <div className="flex h-full w-96 flex-col md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col">
-        <div className='flex w-full items-center justify-between'>
+        <div className="flex w-full items-center justify-between">
           <h2 className="text-3xl">Onchainkit</h2>
           <div className="flex gap-3">
             <SignupButton />
@@ -20,10 +20,10 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section className='flex w-full grow flex-col items-center justify-center gap-4 rounded-xl bg-gray-100'>
-        <div className='flex h-[450px] w-[450px] items-center justify-center rounded-xl bg-[#030712]'>
-          <div className='rounded-xl bg-[#F3F4F6] px-4 py-[11px]'>
-            <p className='font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]'>
+      <section className="flex w-full grow flex-col items-center justify-center gap-4 rounded-xl bg-gray-100">
+        <div className="flex h-[450px] w-[450px] items-center justify-center rounded-xl bg-[#030712]">
+          <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
+            <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
               npm install @coinbase/onchainkit
             </p>
           </div>
@@ -34,11 +34,11 @@ export default function Page() {
           <WalletWrapper className="w-[450px]" text="Sign in to collect" />
         )}
       </section>
-      <section className='mt-12 mb-6 flex w-full justify-between'>
+      <section className="mt-12 mb-6 flex w-full justify-between">
         <aside className="flex items-center">
-          <h3 className='mr-2 text-m'>Built with love on</h3>
+          <h3 className="mr-2 text-m">Built with love on</h3>
           <BaseSvg />
-          <h3 className='ml-1 text-[#0052FF] text-m'>Base</h3>
+          <h3 className="ml-1 text-[#0052FF] text-m">Base</h3>
         </aside>
         <ul className="flex gap-6">
           <li className="flex">
@@ -47,19 +47,28 @@ export default function Page() {
             </a>
           </li>
           <li className="flex">
-            <a href="https://github.com/coinbase/onchainkit" target="_blank" rel="noreferrer">
+            <a
+              href="https://github.com/coinbase/onchainkit"
+              target="_blank"
+              rel="noreferrer"
+            >
               Github
             </a>
           </li>
           <li className="flex">
-            <a href="https://discord.gg/8gW3h6w5" target="_blank" rel="noreferrer">
+            <a
+              href="https://discord.gg/8gW3h6w5"
+              target="_blank"
+              rel="noreferrer"
+            >
               Discord
             </a>
           </li>
           <li className="flex">
             <a
               href="https://www.figma.com/community/file/1370194397345450683/onchainkit"
-              target="_blank" rel="noreferrer"
+              target="_blank"
+              rel="noreferrer"
             >
               Figma
             </a>
