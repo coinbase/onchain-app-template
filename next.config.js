4af9be6af35be07e7c847543429edf80b0ab6eab
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2024-08-07 11:31:00 -0700

    dope

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    WALLET_CONNECTOR_PROJECT_ID: process.env.WALLET_CONNECTOR_PROJECT_ID,
-  },
-};
-
-module.exports = nextConfig;
