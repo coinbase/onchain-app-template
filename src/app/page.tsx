a7b3af0362b2664b12f4042d14cf2e7697a75472
Alissa Crane <alissa.crane@coinbase.com>
2024-08-07 14:13:57 -0700

    fix imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 import Footer from 'src/components/Footer';
 import TransactionWrapper from 'src/components/TransactionWrapper';
 import WalletWrapper from 'src/components/WalletWrapper';
+import { ONCHAINKIT_LINK } from 'src/links';
 import OnchainkitSvg from 'src/svg/OnchainkitSvg';
 import { useAccount } from 'wagmi';
 import LoginButton from '../components/LoginButton';
 import SignupButton from '../components/SignupButton';
-import { ONCHAINKIT_LINK } from 'src/links';
 
 export default function Page() {
   const { address } = useAccount();
