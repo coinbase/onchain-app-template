3bd03084a958841777dbaf14aa0d01cfc68d0227
Alissa Crane <alissa.crane@coinbase.com>
2024-08-06 17:11:41 -0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    WALLET_CONNECTOR_PROJECT_ID: process.env.WALLET_CONNECTOR_PROJECT_ID,
+  },
+};
 
 module.exports = nextConfig;
