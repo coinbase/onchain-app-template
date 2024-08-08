0c6dcb8adddb8876326d63592bcab9c8642b3ef2
Alissa Crane <alissa.crane@coinbase.com>
2024-08-08 14:35:59 -0700

    swap login and signup button functionality

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -2,5 +2,11 @@
 import WalletWrapper from './WalletWrapper';
 
 export default function LoginButton() {
-  return <WalletWrapper className="min-w-[90px]" text="Log in" />;
+  return (
+    <WalletWrapper
+      className="min-w-[90px]"
+      text="Log in"
+      withWalletAggregator={true}
+    />
+  );
 }
--- src/components/SignupButton.tsx
+++ src/components/SignupButton.tsx
@@ -6,7 +6,6 @@ export default function SignupButton() {
     <WalletWrapper
       className="ockConnectWallet_Container min-w-[90px] shrink bg-slate-200 text-[#030712] hover:bg-slate-300"
       text="Sign up"
-      withWalletAggregator={true}
     />
   );
 }
