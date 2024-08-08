c53cf6ba17152a09382e469e8e09abe365f24fd0
Paul Cramer <paulccramer@gmail.com>
2024-08-07 19:52:30 -0700

    asdf

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_WC_PROJECT_ID: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
-  },
-};
-
-module.exports = nextConfig;
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -10,37 +10,12 @@ import { http, createConfig } from 'wagmi';
 import { baseSepolia } from 'wagmi/chains';
 import { NEXT_PUBLIC_WC_PROJECT_ID } from './config';
 
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended Wallet',
-      wallets: [coinbaseWallet],
-    },
-    {
-      groupName: 'Other Wallets',
-      wallets: [rainbowWallet, metaMaskWallet],
-    },
-  ],
-  {
-    appName: 'onchainkit',
-    projectId: NEXT_PUBLIC_WC_PROJECT_ID || '',
-  },
-);
-
-export const wagmiConfig = createConfig({
-  chains: [baseSepolia],
-  // turn off injected provider discovery
-  multiInjectedProviderDiscovery: false,
-  connectors,
-  ssr: true,
-  transports: {
-    [baseSepolia.id]: http(),
-  },
-});
-
-declare module 'wagmi' {
-  interface Register {
-    config: typeof wagmiConfig;
+export function useWamigConfig() {
+  const projectId = NEXT_PUBLIC_WC_PROJECT_ID ?? '';
+  if (!projectId) {
+    const providerErrMessage =
+      'To connect to all Wallets you need to provide a NEXT_PUBLIC_WC_PROJECT_ID env variable';
+    throw new Error(providerErrMessage);
   }
 
   return useMemo(() => {
