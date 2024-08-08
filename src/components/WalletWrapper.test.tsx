368a8d97441b38e012eb4835f5eabbdd04271ad1
Alissa Crane <alissa.crane@coinbase.com>
2024-08-08 12:42:27 -0700

    add wallet test

--- src/components/WalletWrapper.test.tsx
+++ src/components/WalletWrapper.test.tsx
@@ -1,10 +1,42 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import WalletWrapper from './WalletWrapper';
+import { http, WagmiProvider, createConfig } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { base } from 'wagmi/chains';
+import { mock } from 'wagmi/connectors';
+
+const config = createConfig({
+  chains: [base],
+  connectors: [
+    mock({
+      accounts: [
+        '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+        '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
+        '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+      ],
+    }),
+  ],
+  transports: {
+    [base.id]: http(),
+  },
+});
+const queryClient = new QueryClient();
+
+const renderWithProviders = (Component: React.ComponentType) => {
+  return render(
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component />
+      </QueryClientProvider>
+    </WagmiProvider>,
+  );
+};
 
 describe('WalletWrapper', () => {
   it('should renders', () => {
-    render(<WalletWrapper />);
-    expect(true).toBeTruthy();
+    renderWithProviders(WalletWrapper);
+    const wallet = screen.getByTestId('ockConnectWallet_Container');
+    expect(wallet).toBeInTheDocument();
   });
 });
