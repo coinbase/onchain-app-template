fef1910cbcb19728235c34eef5ea862996634343
Alissa Crane <alissa.crane@coinbase.com>
2024-08-08 11:35:27 -0700

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,13 +23,13 @@ export default function Page() {
           >
             <OnchainkitSvg />
           </a>
-          <div className="flex gap-3 items-center">
+          <div className="flex items-center gap-3">
             <SignupButton />
             {!address && <LoginButton />}
           </div>
         </div>
       </section>
-      <section className="templateSection flex w-full md:grow flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 py-4 px-2">
+      <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
         <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,9 +19,9 @@ const docLinks = [
 
 export default function Footer() {
   return (
-    <section className="mt-auto mb-2 flex w-full flex-col justify-between gap-2 flex-col-reverse md:flex-row md:mt-8 md:mb-6">
+    <section className="mt-auto mb-2 flex w-full flex-col flex-col-reverse justify-between gap-2 md:mt-8 md:mb-6 md:flex-row">
       <aside className="flex items-center pt-2 md:pt-0">
-        <h3 className="mr-2 text-m md:mb-0 mb-2">
+        <h3 className="mr-2 mb-2 text-m md:mb-0">
           Built with love by{' '}
           <a
             href={ONCHAINKIT_LINK}
@@ -34,7 +34,7 @@ export default function Footer() {
           </a>
         </h3>
       </aside>
-      <ul className="flex max-w-full flex-col flex-wrap justify-center gap-3 md:justify-start md:flex-row md:gap-6 mt-4 md:mt-0">
+      <ul className="mt-4 flex max-w-full flex-col flex-wrap justify-center gap-3 md:mt-0 md:flex-row md:justify-start md:gap-6">
         {docLinks.map(({ href, title }) => (
           <li className="flex" key={href}>
             <a
