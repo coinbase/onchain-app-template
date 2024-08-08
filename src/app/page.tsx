e39fadb00aafcb852e00b3c93ae333e58f107c96
Alissa Crane <alissacranespam@gmail.com>
2024-08-08 14:47:01 -0700

    chore: swap login and signup button functionality (#21)
    Co-authored-by: Alissa Crane <alissa.crane@coinbase.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   const { address } = useAccount();
 
   return (
-    <div className="flex h-full w-96 max-w-full flex-col md:w-[1008px]">
+    <div className="flex h-full w-96 max-w-full flex-col px-1 md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col md:flex-row">
         <div className="flex w-full flex-row items-center justify-between gap-2 md:gap-0">
           <a
