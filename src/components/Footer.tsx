0fe73271293872e5ab27a4c628a44e149204f55d
Alissa Crane <alissa.crane@coinbase.com>
2024-08-08 11:11:06 -0700

    adjust mobile styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,9 +19,9 @@ const docLinks = [
 
 export default function Footer() {
   return (
-    <section className="mt-12 mb-6 flex w-full flex-col justify-between gap-2 pb-4 md:flex-row md:pb-0">
-      <aside className="flex items-center justify-center md:justify-start">
-        <h3 className="mr-2 text-m">
+    <section className="mt-auto mb-2 flex w-full flex-col justify-between gap-2 flex-col-reverse md:flex-row md:mt-8 md:mb-6">
+      <aside className="flex items-center pt-2 md:pt-0">
+        <h3 className="mr-2 text-m md:mb-0 mb-2">
           Built with love by{' '}
           <a
             href={ONCHAINKIT_LINK}
@@ -34,7 +34,7 @@ export default function Footer() {
           </a>
         </h3>
       </aside>
-      <ul className="flex max-w-full flex-wrap justify-center gap-3 md:justify-start md:gap-6">
+      <ul className="flex max-w-full flex-col flex-wrap justify-center gap-3 md:justify-start md:flex-row md:gap-6 mt-4 md:mt-0">
         {docLinks.map(({ href, title }) => (
           <li className="flex" key={href}>
             <a
