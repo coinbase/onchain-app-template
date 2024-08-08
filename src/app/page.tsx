195c1c03c86cb80867d2e04324a04be002bc771e
Alissa Crane <alissacranespam@gmail.com>
2024-08-08 11:58:16 -0700

    chore: Adjust mobile styling (#17)
    Co-authored-by: Alissa Crane <alissa.crane@coinbase.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
   return (
     <div className="flex h-full w-96 max-w-full flex-col md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col md:flex-row">
-        <div className="flex w-full flex-col items-center justify-between gap-2 md:flex-row md:gap-0">
+        <div className="flex w-full flex-row items-center justify-between gap-2 md:gap-0">
           <a
             href={ONCHAINKIT_LINK}
             title="onchainkit"
@@ -23,13 +23,13 @@ export default function Page() {
           >
             <OnchainkitSvg />
           </a>
-          <div className="flex gap-3 pt-3 md:pt-0">
+          <div className="flex items-center gap-3">
             <SignupButton />
             {!address && <LoginButton />}
           </div>
         </div>
       </section>
-      <section className="templateSection flex w-full grow flex-col items-center justify-center gap-4 rounded-xl md:bg-gray-100">
+      <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
         <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
