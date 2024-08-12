c3869def16ff6d87d78214d704a272aad6b90375
Alissa Crane <alissa.crane@coinbase.com>
2024-08-12 13:31:09 -0700

    update chain in onchainkit provider

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -3,7 +3,7 @@ import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
-import { base } from 'viem/chains';
+import { baseSepolia } from 'viem/chains';
 import { WagmiProvider } from 'wagmi';
 import { NEXT_PUBLIC_CDP_API_KEY } from '../config';
 import { useWamigConfig } from '../wagmi';
@@ -18,7 +18,7 @@ function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
+        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={baseSepolia}>
           <RainbowKitProvider modalSize="compact">
             {children}
           </RainbowKitProvider>
