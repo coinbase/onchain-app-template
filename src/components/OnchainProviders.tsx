390230be33c841e5265150b6dbfdc0946259697d
Alissa Crane <alissa.crane@coinbase.com>
2024-08-15 20:39:46 -0700

    fix formatting

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -18,10 +18,7 @@ function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey={NEXT_PUBLIC_CDP_API_KEY}
-          chain={base}
-        >
+        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
           <RainbowKitProvider modalSize="compact">
             {children}
           </RainbowKitProvider>
