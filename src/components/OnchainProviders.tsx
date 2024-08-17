80c414c19872e45c44ecc93f36e9ff3ec14d507f
Alissa Crane <alissacranespam@gmail.com>
2024-08-17 11:55:13 -0700

    fix: updated wagmi config and OnchainKitProvider chain (#27)
    Co-authored-by: Alissa Crane <alissa.crane@coinbase.com>

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -3,7 +3,7 @@ import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
-import { baseSepolia } from 'viem/chains';
+import { base } from 'viem/chains';
 import { WagmiProvider } from 'wagmi';
 import { NEXT_PUBLIC_CDP_API_KEY } from '../config';
 import { useWamigConfig } from '../wagmi';
@@ -18,10 +18,7 @@ function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider
-          apiKey={NEXT_PUBLIC_CDP_API_KEY}
-          chain={baseSepolia}
-        >
+        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
           <RainbowKitProvider modalSize="compact">
             {children}
           </RainbowKitProvider>
