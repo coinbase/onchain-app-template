492bd35b256701653da13dbc5a3ac4f51b88bdbf
Paul Cramer <paulccramer@gmail.com>
2025-02-04 17:15:57 -0800

    asfd

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -13,10 +13,8 @@ describe('Footer', () => {
   it('renders the footer component', () => {
     render(<Footer />);
 
-    // Check if "Built with love by" text is present
     expect(screen.getByText(/Built with love by/i)).toBeInTheDocument();
 
-    // Check if OnchainKit link is present
     const onchainKitLink = screen.getByTitle('OnchainKit');
     expect(onchainKitLink).toBeInTheDocument();
     expect(onchainKitLink).toHaveAttribute('href', ONCHAINKIT_LINK);
@@ -33,12 +31,12 @@ describe('Footer', () => {
       { href: TWITTER_LINK, title: 'X' },
     ];
 
-    links.forEach(({ href, title }) => {
+    for (const { href, title } of links) {
       const link = screen.getByTitle(title);
       expect(link).toBeInTheDocument();
       expect(link).toHaveAttribute('href', href);
       expect(link).toHaveAttribute('target', '_blank');
       expect(link).toHaveAttribute('rel', 'noreferrer');
-    });
+    }
   });
 });
